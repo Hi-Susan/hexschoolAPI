07362f7e5810353c4c32fe9f0f5791d78d6a8c68
Susan <susan.huang@tenfoldtech.com>
2020-07-03 20:14:55 +0800

    將函式改為物件

--- build/js/index.js
+++ build/js/index.js
@@ -1,36 +1,39 @@
-const uuid = "79461a77-1e28-45a1-8d5c-dca988a7ae07"
-const apiPath = "https://course-ec-api.hexschool.io/"
 
-function getProducts () {
-  axios
-    .get(`${apiPath}api/${uuid}/ec/products`)
-    .then(function(response) {
-      const myData = response.data.data
-      myData.forEach(element => getProductInfo(element.id))
-    })
-}
-function getProductInfo (id) {
-  axios
-    .get(`${apiPath}api/${uuid}/ec/product/${id}`)
-    .then(function(response) {
-      const myData = response.data.data
-      renderProductList (myData)
-    })
-}
-function renderProductList (data) {
-  // <div class="products-card-tag">8折</div>
-  let templateProductList = `
-  <div>
-    <div class="products-card">
-      <div class="products-card-image" style="background-image: url('${data.imageUrl}')"><a class="products-card-button" href="javascript:;">加入購物車</a></div>
-      <div class="products-card-title">${data.title}</div>
-      <div class="products-card-price">${data.price}</div>
-      <div class="products-card-info">${data.content}</div>
-      <a class="products-card-link" href="javascript:;"></a>
+var products = {
+  uuid: "79461a77-1e28-45a1-8d5c-dca988a7ae07",
+  apiPath: "https://course-ec-api.hexschool.io/",
+  getProducts () {
+    const vm = this;
+    axios
+      .get(`${vm.apiPath}api/${vm.uuid}/ec/products`)
+      .then(function(response) {
+        const myData = response.data.data
+        myData.forEach(element => vm.getProductInfo(element.id))
+      })
+  },
+  getProductInfo (id) {
+    const vm = this;
+    axios
+      .get(`${vm.apiPath}api/${vm.uuid}/ec/product/${id}`)
+      .then(function(response) {
+        const myData = response.data.data
+        vm.renderProductList (myData)
+      })
+  },
+  renderProductList (data) {
+    let templateProductList = `
+    <div>
+      <div class="products-card">
+        <div class="products-card-image" style="background-image: url('${data.imageUrl}')"><a class="products-card-button" href="javascript:;">加入購物車</a></div>
+        <div class="products-card-title">${data.title}</div>
+        <div class="products-card-price">${data.price}</div>
+        <div class="products-card-info">${data.content}</div>
+        <a class="products-card-link" href="javascript:;"></a>
+      </div>
     </div>
-  </div>
-  `
-  document.querySelector('#productsList').innerHTML += templateProductList
+    `
+    document.querySelector('#productsList').innerHTML += templateProductList
+  }
 }
-getProducts();
+products.getProducts();
 
--- dist/js/index.js
+++ dist/js/index.js
@@ -1,31 +1,30 @@
 (function () {
   'use strict';
 
-  var uuid = "79461a77-1e28-45a1-8d5c-dca988a7ae07";
-  var apiPath = "https://course-ec-api.hexschool.io/";
-
-  function getProducts() {
-    axios.get(apiPath + "api/" + uuid + "/ec/products").then(function (response) {
-      var myData = response.data.data;
-      myData.forEach(function (element) {
-        return getProductInfo(element.id);
+  var products = {
+    uuid: "79461a77-1e28-45a1-8d5c-dca988a7ae07",
+    apiPath: "https://course-ec-api.hexschool.io/",
+    getProducts: function getProducts() {
+      var vm = this;
+      axios.get(vm.apiPath + "api/" + vm.uuid + "/ec/products").then(function (response) {
+        var myData = response.data.data;
+        myData.forEach(function (element) {
+          return vm.getProductInfo(element.id);
+        });
       });
-    });
-  }
-
-  function getProductInfo(id) {
-    axios.get(apiPath + "api/" + uuid + "/ec/product/" + id).then(function (response) {
-      var myData = response.data.data;
-      renderProductList(myData);
-    });
-  }
-
-  function renderProductList(data) {
-    // <div class="products-card-tag">8折</div>
-    var templateProductList = "\n  <div>\n    <div class=\"products-card\">\n      <div class=\"products-card-image\" style=\"background-image: url('" + data.imageUrl + "')\"><a class=\"products-card-button\" href=\"javascript:;\">\u52A0\u5165\u8CFC\u7269\u8ECA</a></div>\n      <div class=\"products-card-title\">" + data.title + "</div>\n      <div class=\"products-card-price\">" + data.price + "</div>\n      <div class=\"products-card-info\">" + data.content + "</div>\n      <a class=\"products-card-link\" href=\"javascript:;\"></a>\n    </div>\n  </div>\n  ";
-    document.querySelector('#productsList').innerHTML += templateProductList;
-  }
-
-  getProducts();
+    },
+    getProductInfo: function getProductInfo(id) {
+      var vm = this;
+      axios.get(vm.apiPath + "api/" + vm.uuid + "/ec/product/" + id).then(function (response) {
+        var myData = response.data.data;
+        vm.renderProductList(myData);
+      });
+    },
+    renderProductList: function renderProductList(data) {
+      var templateProductList = "\n    <div>\n      <div class=\"products-card\">\n        <div class=\"products-card-image\" style=\"background-image: url('" + data.imageUrl + "')\"><a class=\"products-card-button\" href=\"javascript:;\">\u52A0\u5165\u8CFC\u7269\u8ECA</a></div>\n        <div class=\"products-card-title\">" + data.title + "</div>\n        <div class=\"products-card-price\">" + data.price + "</div>\n        <div class=\"products-card-info\">" + data.content + "</div>\n        <a class=\"products-card-link\" href=\"javascript:;\"></a>\n      </div>\n    </div>\n    ";
+      document.querySelector('#productsList').innerHTML += templateProductList;
+    }
+  };
+  products.getProducts();
 
 }());
